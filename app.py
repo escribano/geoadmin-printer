0a35cdca30c2300d14f9c4535cad2749b9730e6e
Marc Monnerat <marc.monnerat@bluewin.ch>
2012-10-28 17:36:50 +0100

    add support for image width, height parameter, json and callback response. layout and general cleanup

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import datetime
 import hashlib
 import os
 import subprocess
+import simplejson as json
+from urlparse import urlparse
 
-from flask import Flask, abort, redirect, render_template, request
+from flask import Flask, abort, redirect, render_template, request, jsonify, Response, url_for
 
 
 # app configuration
@@ -27,25 +29,43 @@ def index():
 
 @app.route('/print')
 def printer():
-    
     url = request.args.get('url')
+    height = int(request.args.get('h', 450))
+    width = int(request.args.get('w', 600))
+    print height, width
+    if (height > 1000 and width > 1600):
+        return Response("Bad request", status=400)   
+
+    cb = request.args.get('cb', None)
+    
     if url and "geo.admin.ch" in url:
-        
-        url_hash = hashlib.md5(url).hexdigest()
+        r = "&".join([url,"width=%s" % width, "height=%s" %height])
+        url_hash = hashlib.md5(r).hexdigest()
         filename = 'map-%s.jpeg' % url_hash
         
         outfile = os.path.join(MEDIA_ROOT, 'downloads',filename)
         image = os.path.join(MEDIA_URL, 'downloads',filename)
         
-        if os.path.exists(outfile):
-            return redirect(image)
-        else:
-               
-            params = [PHANTOM, SCRIPT, url, outfile]
-
+        if not os.path.exists(outfile):
+            params = [PHANTOM, SCRIPT, url, outfile, str(width), str(height)]
+        
             exitcode = subprocess.call(params)
-            if exitcode == 0:
-                return redirect(image)
+            if exitcode != 0:
+                return Response("Internal error", status=500)   
+            
+        image_url =  url_for('static', filename=os.path.join('downloads',filename), _external=True)
+        if cb:
+            data = {
+                'image'  : image_url,
+                'width':  width,
+                'height': height
+            }
+            js = "%s( %s)" %  (cb, json.dumps(data))
+
+            return Response(js, status=200, mimetype='application/json')
+        return redirect(image_url)
+
+
             
     abort(404)
 
--- screenshot.js
+++ screenshot.js
@@ -1,53 +1,71 @@
-var page = new WebPage(),
-    address, outfile, width, height, clip_height;
-    
-    //  http://map.geo.admin.ch/?Y=727770&X=163650&zoom=4&bgLayer=ch.swisstopo.pixelkarte-farbe&layers=ch.bafu.schutzgebiete-schweizerischer_nationalpark,ch.bafu.schutzgebiete-paerke_nationaler_bedeutung,ch.bafu.bundesinventare-moorlandschaften,ch.bfe.abgeltung-wasserkraftnutzung,ch.bafu.wasser-leitungen,ch.bafu.wasser-rueckgabe,ch.bafu.wasser-entnahme&layers_opacity=0.75,0.85,0.75,1,1,1,1&layers_visibility=true,true,true,true,true,true,true&lang=fr
-    
-    // http://s.geo.admin.ch/27498582
-    
-    
-   // http://map.geo.admin.ch/?selectedNode=LT1_4&Y=672470&X=172050&zoom=2&bgLayer=ch.swisstopo.pixelkarte-farbe&layers=ch.bafu.wasser-leitungen,ch.bafu.wasser-rueckgabe,ch.bafu.wasser-entnahme&layers_opacity=1,1,1&layers_visibility=true,true,true&lang=fr
+var page = require('webpage').create(),
+system = require('system'),
+address,outfile,width,
+height,clip_height,clip_width;
 
-address = phantom.args[0];
-outfile = phantom.args[1];
+if (system.args.length < 3 || system.args.length > 5) {
+    console.log('Usage: screenshot.js URL filename [width height]');
+    phantom.exit(1);
 
-//height = 800;
+} else {
+
+    address = system.args[1];
+    outfile = system.args[2];
+    clip_width = 600;
+    clip_height = 400;
+    if (system.args.length == 5) {
+        clip_width = parseInt(system.args[3]);
+        clip_height = parseInt(system.args[4]);
+    }
+}
+// clipping parameter to remove map.geo.adminc
 left = 300;
 top = 150;
 bottom = 24;
-clip_height = 400; // = height - top;
-clip_width = 600; //width - left ;
+
 width = clip_width + left;
 height = clip_height + top + bottom;
 
+page.viewportSize = {
+    width: width,
+    height: height
+};
+page.clipRect = {
+    top: top,
+    left: left,
+    width: clip_width,
+    height: clip_height
+};
 
-page.viewportSize = { width: width, height: height };
-page.clipRect = { top: top, left: left, width: clip_width, height: clip_height };
-//page.paperSize = { format: 'A4', orientation: 'landscape', border: '1cm' }
+page.open(address, function(status) {
+    if (status !== 'success') {
+        phantom.exit(1);
+    } else {
+        page.evaluate(function() {
+            try {
+                console.log('hidding');
 
-page.open(address, function (status) {
-  if (status !== 'success') {
-    phantom.exit(1);
-  } else {
-    page.evaluate(function(){
-           try {
-	        console.log('hidding');
+                document.getElementById('side-panel').style.display = 'none';
+                document.getElementById('header').style.display = 'none';
+                document.getElementsByClassName('x-panel-tbar-noheader')[0].style.display = 'none';
+                document.getElementsByClassName('x-panel-tbar-noheader')[1].style.display = 'none';
+                document.getElementsByClassName('x-panel-bbar')[0].style.display = 'none';
+
                 document.getElementsByClassName('olControlPanZoomBar')[0].style.display = "none";
-		document.getElementsByClassName('olControlOverviewMap')[0].style.display = "none";
-		document.getElementsByClassName('olControlPanel')[0].style.display = "none";
-                
-            } catch (e) {
-	      console.log('error');
-               
+                document.getElementsByClassName('olControlOverviewMap')[0].style.display = "none";
+                document.getElementsByClassName('olControlPanel')[0].style.display = "none";
+                /*    var p = document.getElementsByClassName('geoadmin-mappanel')[0];
+                p.style.top = 0;
+                p.style.left = 0;
+                p.style.width = width+"px";
+                p.style.height = height+"px";*/
+
+                } catch(e) {
+                console.log('error');
+
             }
         });
-    
-    
-    
-    page.render(outfile);
-    //var base64image = page.renderBase64('PNG');
-    //var fs = require("fs");
-    //fs.write("/dev/stdout", base64image, "w");
-    phantom.exit();
-  }
+        page.render(outfile);
+        phantom.exit();
+    }
 });
